ba609043e911682615acdd929eb10f3babd1e2d9
Alberto Alan Sánchez <al404015@uji.es>
2024-02-27 10:23:48 +0100

    wip change contrls menu

---
+++ Assets/Scripts/Options/OptionsCanva.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class OptionsCanva : MonoBehaviour
+{
+    /*public GameObject PJ;
+    private PlayerInput _playerInput = null;
+    
+    private void Awake()
+    {
+        if(PJ!=null) _playerInput = PJ.GetComponent<PlayerInput>();
+        else Debug.Log("No PJ");
+    }
+
+    private void OnEnable()
+    {
+        if(_playerInput!=null) _playerInput.enabled = false;
+        else Debug.Log("1 No player input");
+    }
+
+    private void OnDisable()
+    {
+        if(_playerInput!=null) _playerInput.enabled = false;
+        else Debug.Log("2 No player input");
+
+        
+    }*/
+}
