3d9dd9538037ab689f1e6b03a893ad578ad53156
AlbertoAlanSA <83225413+AlbertoAlanSA@users.noreply.github.com>
2024-06-10 22:12:10 +0200

    delete anim pjk

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -6,7 +6,7 @@ public class PlayerManager : MonoBehaviour
     public InputReader inputReader;
     public Rigidbody2D rb;
     public DialogueManager _dialogueManager;
-
+    
     private ArticyObject availableDialogue;
 
     private MenuManager _menuManager;
